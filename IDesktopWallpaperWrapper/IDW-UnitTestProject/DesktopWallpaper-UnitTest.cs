4442b18fcc75336c89b7ca710215bc0ceab2c9d9
9eck0 <rui.jie.liu@umontreal.ca>
2022-04-11 15:40:46 -0400

    Added unit testing project with sample testing

---
+++ IDesktopWallpaperWrapper/IDW-UnitTestProject/DesktopWallpaper-UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using IDesktopWallpaperWrapper;
+
+namespace IDW_UnitTestProject
+{
+    [TestClass]
+    public class DesktopWallpaperUnitTest
+    {
+        [TestMethod]
+        public void TestGetWallpaper()
+        {
+            DesktopWallpaper desktopWallpaper = new DesktopWallpaper();
+
+            // A call to GetActiveMonitorIDs() will test all monitor-related methods.
+            string[] activeMonitorIDs = desktopWallpaper.GetActiveMonitorIDs();
+            Assert.IsNotNull(activeMonitorIDs, "GetActiveMonitorIDs() returned a null array of monitor IDs");
+            Assert.IsTrue(activeMonitorIDs.Length > 0, "GetActiveMonitorIDs() returned an empty array of monitor IDs");
+
+            foreach (string monitorID in activeMonitorIDs) {
+                string imageLocation = desktopWallpaper.GetWallpaper(monitorID);
+                Assert.IsNotNull(imageLocation, "GetWallpaper() returned a null string path");
+                Assert.IsTrue(imageLocation.Length > 0, "GetWallpaper() returned an empty string path");
+            }
+        }
+
+        [TestMethod]
+        public void TestGetSlideshow()
+        {
+
+        }
+    }
+}
